8c3073764741aba588e5c42a922a0e14adeb0856
유승훈 <a5750791@gmail.com>
2026-02-22 20:08:39 +0900

    로컬 테스트 시 docker-compose down 명령어 실행되지 않던 버그 수정

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+cleanup() {
+    echo "컨테이너 정리 중..."
+    docker-compose down
+}
+trap cleanup EXIT INT TERM
+
 docker-compose down
 rm -rf ./reports
 
@@ -8,5 +14,3 @@ docker-compose up --build --abort-on-container-exit
 
 allure generate ./reports/allure-results -o ./reports/allure-report
 allure open ./reports/allure-report
-
-docker-compose down
